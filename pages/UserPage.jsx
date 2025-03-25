3cc38501d40a516f8ee98135be6c6c421a88d4be
Yurii <yura.huzienko@gmail.com>
2025-03-25 17:36:03 +0200

    changes, full userpage

--- pages/UserPage.jsx
+++ pages/UserPage.jsx
@@ -1,18 +1,12 @@
 import { Button } from "@rneui/base";
-import {
-  Image,
-  Pressable,
-  SafeAreaView,
-  StyleSheet,
-  Text,
-  View,
-} from "react-native";
+import { Image, SafeAreaView, StyleSheet, Text, View } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import Sms from "../components/svg/Sms";
 import Phone from "../components/svg/Phone";
 import Email from "../components/svg/Email";
 import Info from "../components/svg/Info";
 import Arrow from "../components/svg/Arrow";
+import { ScrollView } from "react-native";
 const UserPage = () => {
   const insets = useSafeAreaInsets();
 
@@ -21,68 +15,71 @@ const UserPage = () => {
       <SafeAreaView
         style={{
           marginTop: -insets.top,
-          backgroundColor: "tomato",
+          backgroundColor: "#13293D",
         }}
       >
-        <Arrow style={{ paddingLeft: "20%", marginTop: "20%" }}></Arrow>
+        <Arrow
+          stroke="#FCF7F8"
+          style={{ paddingLeft: "20%", marginTop: "15%" }}
+        ></Arrow>
         <View style={styles.circleContainer}>
           <Image source={require("../assets/123.jpg")} style={styles.image} />
           <Button
             title={"Upload new photo"}
-            radius={10}
             type="clear"
-            color={"#ffffff"}
             buttonStyle={{
-              marginBottom: 5,
+              marginBottom: 4,
               alignSelf: "flex-start",
               textAlign: "center",
-              paddingTop: 25,
+              paddingTop: 15,
             }}
             titleStyle={{
-              fontSize: 20,
-              color: "black",
+              fontSize: 14,
+              color: "#FCF7F8",
               letterSpacing: 1.5,
             }}
           />
         </View>
       </SafeAreaView>
-      <View style={styles.container}>
-        <View style={styles.textContainer}>
-          <Sms />
-          <View style={styles.textContainer2}>
-            <Text style={styles.header}>Full name</Text>
-            <Text style={styles.text}>huy</Text>
+      <ScrollView vertical={true}>
+        <View style={styles.container}>
+          <View style={styles.textContainer}>
+            <Sms />
+            <View style={styles.textContainer2}>
+              <Text style={styles.header}>Full name</Text>
+              <Text style={styles.text}>Sava Neger</Text>
+            </View>
+          </View>
+          <View style={styles.textContainer}>
+            <Phone></Phone>
+            <View style={styles.textContainer2}>
+              <Text style={styles.header}>Phone number</Text>
+              <Text style={styles.text}>333222111</Text>
+            </View>
+          </View>
+          <View style={styles.textContainer}>
+            <Email></Email>
+            <View style={styles.textContainer2}>
+              <Text style={styles.header}>Email</Text>
+              <Text style={styles.text}>sava@gmail.com</Text>
+            </View>
+          </View>
+          <View style={styles.textContainer}>
+            <Info></Info>
+            <View style={styles.textContainer3}>
+              <Button
+                title={"Personal data"}
+                type="clear"
+                titleStyle={{
+                  fontSize: 22,
+                  color: "black",
+                  fontWeight: "bold",
+                }}
+              />
+            </View>
           </View>
         </View>
-        <View style={styles.textContainer}>
-          <Phone></Phone>
-          <View style={styles.textContainer2}>
-            <Text style={styles.header}>Phone number</Text>
-            <Text style={styles.text}>huy</Text>
-          </View>
-        </View>
-        <View style={styles.textContainer}>
-          <Email></Email>
-          <View style={styles.textContainer2}>
-            <Text style={styles.header}>Email</Text>
-            <Text style={styles.text}>huy@gmail.com</Text>
-          </View>
-        </View>
-        <View style={styles.textContainer}>
-          <Info></Info>
-          <View style={styles.textContainer2}>
-            <Button
-              title={"Personal data"}
-              type="clear"
-              titleStyle={{
-                fontSize: 26,
-                color: "black",
-                fontWeight: "bold",
-              }}
-            />
-          </View>
-        </View>
-      </View>
+      </ScrollView>
     </View>
   );
 };
@@ -95,37 +92,39 @@ const styles = StyleSheet.create({
   },
   container: {
     flex: 1,
-    width: "85%",
+    width: "90%",
     marginTop: 0,
     marginBottom: 0,
     marginHorizontal: "auto",
   },
   image: {
-    width: 150,
-    height: 150,
+    width: 140,
+    height: 140,
     borderRadius: 75,
     overflow: "hidden",
     resizeMode: "cover",
-    marginTop: 5,
   },
   circleContainer: {
     alignItems: "center",
   },
   header: {
-    fontSize: 14,
+    fontSize: 13,
     color: "grey",
   },
   text: {
-    fontSize: 26,
+    fontSize: 22,
     fontWeight: "bold",
   },
   textContainer: {
     display: "flex",
     flexDirection: "row",
     alignItems: "center",
-    paddingTop: "12%",
+    paddingTop: "7%",
   },
   textContainer2: {
-    paddingLeft: 16,
+    paddingLeft: 20,
+  },
+  textContainer3: {
+    paddingLeft: 15,
   },
 });
